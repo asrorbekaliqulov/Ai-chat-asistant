de6963032a065810f4cf22bf0681c4181f6ccfb0
Asrorbek <151444045+asrorbekaliqulov@users.noreply.github.com>
2026-03-28 16:32:49 +0000

    Finish

--- apps/Bot/keybaords.py
+++ apps/Bot/keybaords.py
@@ -2,8 +2,8 @@ from telegram import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 
 admin_keyboardd = [
     [KeyboardButton("🛍 Sotish")],
-    [KeyboardButton(text="📥 Qaytarish"), KeyboardButton(text="➕ Qo'shish")],
-    [KeyboardButton(text="🔍 Qidirish"), KeyboardButton(text="📚 Katalog")],
+    [KeyboardButton(text="🔍 Qidirish"), KeyboardButton(text="➕ Qo'shish")], 
+    # [KeyboardButton(text="📥 Qaytarish"), KeyboardButton(text="📚 Katalog")]
     [KeyboardButton(text="📊 Tahlil")]
 ]
 ADMIN_KYB = ReplyKeyboardMarkup(admin_keyboardd, resize_keyboard=True)
