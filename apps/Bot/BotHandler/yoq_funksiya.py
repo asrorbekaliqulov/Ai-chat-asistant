9a3a3ee4d121a81a2a370fb7e8abfeb705cfaeff
Asrorbek <151444045+asrorbekaliqulov@users.noreply.github.com>
2026-04-17 13:48:18 +0000

    shunchaki

--- apps/Bot/BotHandler/yoq_funksiya.py
+++ apps/Bot/BotHandler/yoq_funksiya.py
@@ -2,4 +2,4 @@ from telegram import Update
 from telegram.ext import ContextTypes
 
 async def yoqfunksiya(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Botga faqat matnli xabar yuboring.")
+    return
